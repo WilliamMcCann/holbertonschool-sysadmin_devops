bc73db0182f44ccc9b8cc5bf79626af1574d70f8
William McCann <william.mccann@holbertonschool.com>
2016-06-22 13:04:01 -0700

    adding fabfile.py

---
+++ deploy_fast_deploy_well/fabfile.py
@@ -0,0 +1,14 @@
+from fabric.api import *
+
+def uptime():
+	run('uptime')
+
+'''def deploy():
+	run(ssh admin@158.69.80.171)
+	local(cd /var/www/html)
+	local(git clone git@github.com:WilliamMcCann/twitter-clone-Ian-William.git)
+
+command1 = 'ssh admin@158.69.80.171'
+
+def access_server():
+	run(command1)'''
