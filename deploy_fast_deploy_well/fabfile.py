d1d186a8fd5376919448b9fc60a3396dd716cd45
William McCann <nwilliammccann@gmail.com>
2016-06-29 19:31:40 -0700

    adding all

--- deploy_fast_deploy_well/fabfile.py
+++ deploy_fast_deploy_well/fabfile.py
@@ -1,16 +1,9 @@
 from fabric.api import *
+from fabric.context_managers import cd
 
 env.use_ssh_config = True
+env.hosts = ('158.69.80.171')
 
-def uptime():
-	run('uptime')
-
-'''def deploy():
-	run(ssh admin@158.69.80.171)
-	local(cd /var/www/html)
-	local(git clone git@github.com:WilliamMcCann/twitter-clone-Ian-William.git)
-
-command1 = 'ssh admin@158.69.80.171'
-
-def access_server():
-	run(command1)'''
+def deploy():
+	with cd('/var/www/html/twitter-clone-Ian-William'):
+		run('git pull')
