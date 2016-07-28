e8e576a1b9a8d7db516c5bf405a9030243e72850
William McCann <nwilliammccann@gmail.com>
2016-07-27 17:17:24 -0700

    final push

--- chef_is_fun/cookbooks/holberton-wrapper/recipes/default.rb
+++ chef_is_fun/cookbooks/holberton-wrapper/recipes/default.rb
@@ -7,84 +7,17 @@
 # All rights reserved - Do Not Redistribute
 #
 #
-
-#maybe this installs apt
-
 include_recipe 'dotdeb'
 
 include_recipe 'ntp::default'
 
-include_recipe 'openssh::default'
+file /var/run/sshd
+
+node.default['openssh']['server']['permit_root_login'] = 'no'
+node.default['openssh']['server']['password_authentication'] = 'no'
 
 include_recipe 'holberton_user'
 
 include_recipe 'nginx'
-
-package 'net-tools'
-
-
-
-
-
-
-
-
-#maybe this part configures a user called "holberton" to not be able to log
-#in using a password to authenticate, only ssh
-
-#adds Dotdeb repo to sources.list and installs GnuPG key
-file '/etc/apt/sources.list' do
-	#run?
-	deb http://packages.dotdeb.org jessie all
-	deb-src http://packages.dotdeb.org jessie all
-	wget https://www.dotdeb.org/dotdeb.gpg
-	sudo apt-key add dotdeb.gpg
-end
-
-"openssh": {
-  "holberton": {
-    "password_authentication": "no"
-  }
-#maybe this line doesn't let you log in as root
-"root": {
-    "user_known_hosts_file": "/dev/null",
-    "strict_host_key_checking": "no"
-  }
-}
-
-begin
-  r = resources(service: 'ssh')
-  if 'debian' == node['platform']
-    r.provider = Chef::Provider::Service::Debian
-  end
-end
-
-user_account 'holberton' do
-	ssh_keygen true
-end
-
-package 'nginx@1.10.1'
-
-service 'nginx' do
-  supports :status => true
-  action [:enable, :start]
-end
-
-
-#maybe this part installs nginx using package and service
-apt_repository 'nginx@1.10.1' do
-	uri	'http://packages.dotdeb.org'
-end
-
-service 'nginx' do
-	action [:enable, :start]
-end
-
-#override service
-#service 'nginx' do
-#  ...
-#  provider Chef::Provider::Service::Debian
-#  ...
-#end
 
 package 'net-tools'
